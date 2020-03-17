174bbe863c5f1cd2732b7adf5659caee31f078e5
Andy Smith <andysmith836@gmail.com>
2020-03-17 22:24:01 +0000

    Get n moves working

--- src/main/java/com/andy/MarsRover.java
+++ src/main/java/com/andy/MarsRover.java
@@ -4,10 +4,15 @@ class MarsRover {
     public MarsRover(Grid grid) {
     }
 
-    public String execute(String command) {
-        if (command.equals("M")) {
-            return "0:1:N";
+    public String execute(String commands) {
+        int numberOfMoves = 0;
+        String[] splitCommands = commands.split("");
+        for (String command : splitCommands) {
+            if (command.equals("M")) {
+                numberOfMoves++;
+            }
         }
-        return "0:0:N";
+
+        return "0:" + numberOfMoves + ":N";
     }
 }
--- src/test/java/com/andy/MarsRoverTest.java
+++ src/test/java/com/andy/MarsRoverTest.java
@@ -15,6 +15,11 @@ public class MarsRoverTest {
         assertEquals(moveRover("M"), "0:1:N");
     }
 
+    @Test
+    public void shouldMoveNorthTwoSquares() {
+        assertEquals(moveRover("MM"), "0:2:N");
+    }
+
     private String moveRover(String command) {
         Grid grid = new Grid();
         MarsRover marsRover = new MarsRover(grid);
