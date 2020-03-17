6d56fa3ebf30f9e84bdb4e4269673a991d5bd61b
Andy Smith <andysmith836@gmail.com>
2020-03-17 22:18:17 +0000

    Clean up tests

--- src/test/java/com/andy/MarsRoverTest.java
+++ src/test/java/com/andy/MarsRoverTest.java
@@ -7,17 +7,17 @@ import static org.junit.Assert.assertEquals;
 public class MarsRoverTest {
     @Test
     public void shouldNotMove() {
-        Grid grid = new Grid();
-        MarsRover marsRover = new MarsRover(grid);
-        String result = marsRover.execute("");
-        assertEquals(result, "0:0:N");
+        assertEquals(moveRover(""), "0:0:N");
     }
 
     @Test
     public void shouldMoveNorthOneSquare() {
+        assertEquals(moveRover("M"), "0:1:N");
+    }
+
+    private String moveRover(String command) {
         Grid grid = new Grid();
         MarsRover marsRover = new MarsRover(grid);
-        String result = marsRover.execute("M");
-        assertEquals(result, "0:1:N");
+        return marsRover.execute(command);
     }
 }
