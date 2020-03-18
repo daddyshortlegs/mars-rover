b50c0063df4acc73e42067951322ffb48a9d1dc0
Andy Smith <andysmith836@gmail.com>
2020-03-18 21:18:35 +0000

    Move coordinate logic into enum

--- src/main/java/com/andy/Bearing.java
+++ src/main/java/com/andy/Bearing.java
@@ -20,17 +20,14 @@ class Bearing {
     public void turnLeft() {
         if (compass == Compass.North) {
             compass = Compass.West;
-            coordinate = new Coordinate(-1, 0);
         } else if (compass == Compass.West) {
             compass = Compass.South;
-            coordinate = new Coordinate(0, -1);
         } else if (compass == Compass.South) {
             compass = Compass.East;
-            coordinate = new Coordinate(1, 0);
         } else if (compass == Compass.East) {
             compass = Compass.North;
-            coordinate = new Coordinate(0, 1);
         }
+        coordinate = compass.getCoordinate();
     }
 
     @Override
