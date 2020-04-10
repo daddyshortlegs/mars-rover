090ff1c9fd402ed7f30ca5540c53cc6777b5c14e
Andy Smith <andysmith836@gmail.com>
2020-04-10 21:54:39 +0100

    Remove traces of Compass

--- src/main/java/com/andy/Grid.java
+++ src/main/java/com/andy/Grid.java
@@ -2,18 +2,15 @@ package com.andy;
 
 class Grid {
     private final Coordinate coordinate = new Coordinate(0, 0);;
-    private Compass compass = Compass.North;
 
     private Direction direction = new North();
 
     void turnRight() {
         direction.turnRight(this);
-        compass = compass.turnRight();
     }
 
     public void turnLeft() {
         direction.turnleft(this);
-        compass = compass.turnLeft();
     }
 
     public void move() {
